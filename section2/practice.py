84ac60312d8d41e06f5af66b7fd1c8fe43afd357
Ted <shinsuengju1@gmail.com>
2025-03-18 13:51:40 +0900

    first commit

---
+++ section2/practice.py
@@ -0,0 +1,28 @@
+# 숫자 자료형
+print(1)
+print(-2)
+print(3.14)
+print(1000)
+print(5+4)
+print(3*2)
+print(4*(3+1))
+
+# 문자열 자료형
+print('안녕')
+print("하이")
+print('ㅎㅎㅎ')
+print('ㅋ'*9)
+
+# boolean 자료형 - 참 / 거짓
+print(5 > 10)  # False
+print(5 < 10)  # True
+print(True)  # True
+print(False)  # False
+print(not True)  # False
+print(not False)  # True
+print(not (5 > 10))  # True
+
+# 애완동물을 소개해 주세요~
+print("우리집 강이지의 이름은 연탄이예요")
+print("연탄이는 4살이며, 산책을 아주 좋아해요")
+print("연탄이는 어른일까요 ? True")
