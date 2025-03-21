36e1b241ac955607b00371473b34b73dd2de978a
Ted <shinsuengju1@gmail.com>
2025-03-21 14:03:39 +0900

    string 빈칸뚫기

---
+++ tutor/string_studentOffer/01.string.py
@@ -0,0 +1,109 @@
+# 문자열 자료형
+
+# 1. 타입 확인
+a = "Life is too short, You need Python"
+# print(여기에 작성)
+# 출력 결과물
+# <class 'str'>
+
+# 2. 문자열 안에 작은따옴표나 큰따옴표를 포함시키고 싶을 때
+# say = 여기에 작성
+# print(say)
+# 출력 결과물
+# "Python is very easy." he says.
+
+
+# food = 여기에 작성
+# print(food)
+# 출력 결과물
+# Python's favorite food is perl
+
+# 3. 여러 줄인 문자열을 변수에 대입하고 싶을 때
+# multiline = 여기에 한줄로 작성
+# print(multiline)
+# 출력 결과물
+# Life is too short
+# You need python
+
+# 4. 문자열 더해서 연결하기
+head = "Python"
+tail = " is fun!"
+# print(여기에 작성)
+# 출력 결과물
+# Python is fun!
+
+# 5. 문자열 곱하기
+head = "Python"
+# print(여기에 작성)
+# 출력 결과물
+# PythonPythonPython!
+
+# 6. 문자열 곱하기를 응용하기
+# print(여기에 작성)
+print("My Program")
+# print(여기에 작성)
+# 출력 결과물 (= 이 50번 사용 되었음)
+# ==================================================
+# My Program
+# ==================================================
+
+# 7. 문자열 길이 구하기
+string = "Life is too short, You need Python"
+# print(여기에 작성)
+# 출력 결과물
+# 34
+
+# 8. 문자열 인덱싱과 슬라이싱 - 인덱스는 0번 부터 시작
+str = "Life is too short, You need Python"
+# 예시 - str[0] 이렇게
+# print(여기에 작성)
+# 출력 결과물
+# e
+
+# 아래 print에는 인덱스를 활용할 것 str[-1] 이런식으로
+# print(여기에 작성)
+# 출력 결과물
+# o
+
+
+# 9. 문자열 슬라이싱
+string = "Life is too short, You need Python"
+# 예시, result = string[5] + string[6]
+# print(여기에 작성)
+# 출력 결과물
+# Life
+
+# 위에를 문자열 슬라이싱을 아래 표현으로 쉽게 슬라이싱 가능
+# a[ : ]
+# a[이상:미만:간격]
+
+# print(여기에 작성)  # 0부터 4이전인 3까지 출력
+# 출력 결과물
+# Life
+
+# print(여기에 작성)  # 뒤에를 생략하면 끝까지 출력
+# 출력 결과물
+# You need Python
+
+# print(여기에 작성)  # [::2] 처음부터 끝까지 2칸 간격으로 출력
+# 출력 결과물
+# Lf stosot o edPto
+
+# print(여기에 작성)  # 19번부터 -7 이전까지 (-7이 Python 앞에 공백이니 d까지 출력)
+# 출력 결과물
+# You need
+
+# print(여기에 작성)  # 역순으로 출력
+# 출력 결과물
+# nohtyP deen uoY ,trohs oot si efiL
+
+
+# 10. 문자열 슬라이싱 활용
+data = "20250318Rainy"
+# date = data[여기에 작성]
+# weather = data[여기에 작성]
+# print(date)
+# print(weather)
+# 출력 결과물
+# 20250318
+# Rainy
---
+++ tutor/string_studentOffer/02.stringFormat.py
@@ -0,0 +1,50 @@
+# 문자열 포매팅 따라 하기
+'''
+문자열 포맷 코드
+%s	문자열(String)
+%c	문자 1개(character)
+%d	정수(Integer)
+%f	부동소수(floating-point)
+%o	8진수
+%x	16진수
+%%	Literal % (문자 % 자체)'
+'''
+# 1. 숫자 바로 대입 (%d 사용)
+# string = 여기에 작성
+# print(string)
+# 출력 결과물
+# I eat 3 apples.
+
+# 2. 문자열 바로 대입
+# string = 여기에 작성
+# print(string)
+# 출력 결과물
+# I eat five apples.
+
+# 3. 숫자 값을 나타내는 변수로 대입
+# number = 여기에 작성
+# string = 여기에 작성
+# print(string)
+# 출력 결과물
+# I eat 3 apples.
+
+# 4. 2개 이상의 값 넣기
+# number = 여기에 작성
+# day = 여기에 작성
+# string = 여기에 작성
+# print(string)
+# 출력 결과물
+# I ate 10 apples. so I was sick for three days.
+
+# 5. 포매팅 연산자 %d와 %를 같이 쓸 때는 %%를 쓴다
+# string = 여기에 작성
+# print(string)
+# 출력 결과물
+# Error is 98%.
+
+
+# 6. 소수점 표현하기
+# number = "여기에 작성" % 3.42134234  # 소수점 넷째자리 까지 표시하도록
+# print(string)
+# 출력 결과물
+# 3.4213
